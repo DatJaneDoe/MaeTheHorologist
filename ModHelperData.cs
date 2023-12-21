1c55fdebb2315482560b6851496c0e0f2f83f40d
DatJaneDoe <54993349+DatJaneDoe@users.noreply.github.com>
2023-12-21 14:25:22 -0500

    Update ModHelperData.cs

--- ModHelperData.cs
+++ ModHelperData.cs
@@ -3,7 +3,7 @@ namespace TimeHero;
 public static class ModHelperData
 {
     public const string WorksOnVersion = "40.0";
-    public const string Version = "1.10";
+    public const string Version = "1.1.0";
     public const string Name = "Mae the Timekeeper";
 
     public const string Description = "Adds a hero with rewinding projectiles and creates damage tracking timerifts!";
