75e4753fcef24045820535fafe31527d423b1357
DatJaneDoe <54993349+DatJaneDoe@users.noreply.github.com>
2023-12-21 14:28:14 -0500

    Update ModHelperData.cs

--- ModHelperData.cs
+++ ModHelperData.cs
@@ -6,9 +6,9 @@ public static class ModHelperData
     public const string Version = "1.1.0";
     public const string Name = "Mae the Timekeeper";
 
-    public const string Description = "Adds a hero with rewinding projectiles and creates damage tracking timerifts!";
+    public const string Description = "Adds a hero with rewinding projectiles and creates damage tracking timerifts! Art and Original Idea By Amber610";
 
-    public const string Icon = "pack1";
+    public const string Icon = "mae";
     public const string RepoOwner = "DatJaneDoe";
     public const string RepoName = "MaeTheHorologist;
     
